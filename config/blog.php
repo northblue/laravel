bc34f2cf02514b8679f5fcd1d3b865965c47ec81
Bin Li <northblue@gmail.com>
2019-06-12 23:42:55 +1000

    a working blog version

---
+++ config/blog.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'name' => "Laravel 学院",
+    'title' => "Laravel 学院",
+    'subtitle' => 'http://laravelacademy.org',
+    'description' => 'Laravel学院致力于提供优质Laravel中文学习资源',
+    'author' => '学院君',
+    'page_image' => 'home-bg.jpg',
+    'posts_per_page' => 10,
+    'uploads' => [
+        'storage' => 'public',
+        'webpath' => '/storage/uploads',
+    ],
+];
